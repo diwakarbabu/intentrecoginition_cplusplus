2c2d040e272488e1e29ee418c5768379f9e74dae
diwakarbabu <apdiwakarbabu@gmail.com>
2021-04-17 16:32:35 +0200

    Updated with time regex

--- header.h
+++ header.h
@@ -16,7 +16,7 @@ regex city("in|[cC]ity");
 regex date("((0[1-9]|[12][0-9]|3[0-1])|([1-9]|[12][0-9]|3[0-1]))[./-]((0[1-9]|1[012])|([1-9]|1[012]))[./-](2021)");
 
 //To check the presence of time - any format
-regex time("([0-2][0-9]:[0-5][0-9]|[0-2][0-9]am|[0-2][0-9]pm|[0-2][0-9]AM|[0-2][0-9]PM|[0-2][0-9]Am|[0-2][0-9]Pm|[0-2][0-9] am|[0-2][0-9] pm|[0-2][0-9] AM|[0-2][0-9] PM|[0-2][0-9] Am|[0-2][0-9] Pm)");
+//regex time("([0-2][0-9]:[0-5][0-9]|[0-2][0-9]am|[0-2][0-9]pm|[0-2][0-9]AM|[0-2][0-9]PM|[0-2][0-9]Am|[0-2][0-9]Pm|[0-2][0-9] am|[0-2][0-9] pm|[0-2][0-9] AM|[0-2][0-9] PM|[0-2][0-9] Am|[0-2][0-9] Pm)");
 
 //To check calander - keywords include Busy, Free, Appointment
 regex check("[bB]usy|[fF]ree|[aA]ppointment");
